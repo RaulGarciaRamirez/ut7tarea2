87329d19b038be4c443df88957842895b8f50715
Raúl <raulgr74@educastur.es>
2022-04-27 12:51:45 +0200

    Commit inicial

---
+++ src/TestFactura.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class TestFactura {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void testIvaredu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIva() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDescuentoesp() {
+		fail("Not yet implemented");
+	}
+
+}
