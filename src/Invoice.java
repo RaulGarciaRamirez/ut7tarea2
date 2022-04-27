fc06fdef5033a922954918ebc91b080d48ff04f8
Raúl <raulgr74@educastur.es>
2022-04-27 13:13:47 +0200

    Encapsulación hecha

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,6 +1,7 @@
 
 /**
  * Clase para hacer facturas y aplicar iva
+ * 
  * @author Programador Bajeril
  *
  */
@@ -10,33 +11,38 @@ public class Invoice {
 	private static final double REDUCED_IVA = 0.21d;
 	double total;
 	double subtotal;
-	
-	public Invoice (double subtotal) {
+
+	public Invoice(double subtotal) {
 		this.subtotal = subtotal;
-	}
-	
-	/**
-	 *  le aplica al total el iva reducido
-	 *  y se guarda en el total
-	 */
-	public void reducedIva () {
-		total = subtotal + (subtotal * REDUCED_IVA);
 	}
 
 	/**
-	 *  le aplica al total el iva normal
-	 *  y se guarda en el total
+	 * le aplica al total el iva reducido y se guarda en el total
 	 */
-	public void iva () {
-		total = subtotal + (subtotal * IVA);		
+	public void reducedIva() {
+		total = applyIva(REDUCED_IVA);
 	}
-	
+
 	/**
-	 * le aplica un descuento especial fijo
-	 * que se aplica a clientes VIP
+	 * le aplica al total el iva normal y se guarda en el total
 	 */
-	public void specialDiscount () {
-		total = subtotal - (subtotal * SPECIAL_DISCOUNT);
+	public void iva() {
+		total = applyIva(IVA);
+	}
+
+	/**
+	 * le aplica un descuento especial fijo que se aplica a clientes VIP
+	 */
+	public void specialDiscount() {
+		total = applyDiscount();
+	}
+
+	private double applyDiscount() {
+		return subtotal - (subtotal * SPECIAL_DISCOUNT);
+	}
+
+	private double applyIva(double ivaType) {
+		return subtotal + (subtotal * ivaType);
 	}
 
 }
